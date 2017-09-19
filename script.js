aacbccb7eee0e6a7501f481baca4d938ebe7caab
Mack Bryan <30667892+MackBryan@users.noreply.github.com>
2017-09-18 19:03:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+window.onload = function(){
+var logo = document.getElementById("logo");
+  logo.addEventListener("click", function(){
+    if(logo.className == "skew"){
+     logo.classList.remove("skew"); 
+    }else{
+      logo.className = "skew";
+    }
+  });
+  
+};
